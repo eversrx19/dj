57069840fdf8f5d27f1518ead781d071d98e5739
eversrx19 <eversrx19@gmail.com>
2022-06-29 18:35:08 +0900

    first commit

---
+++ docker_dj/views.py
@@ -0,0 +1,9 @@
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+
+class PingView(APIView):
+
+    def get(self, request):
+
+        return Response({"pong"})
